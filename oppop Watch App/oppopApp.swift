e7b1b01af9ae568fc2bf633d013cb5e4594c13b5
roudYStrom <anzhelikaroud@yandex.ru>
2024-04-12 15:01:53 +0300

    Initial Commit

---
+++ oppop Watch App/oppopApp.swift
@@ -0,0 +1,17 @@
+//
+//  oppopApp.swift
+//  oppop Watch App
+//
+//  Created by Yulianna on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct oppop_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
